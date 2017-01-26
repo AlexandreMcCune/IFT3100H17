9f7632348d7d97bdeca12d9b4e3a20fffe311cd7
philvoyer <philvoyer@gmail.com>
2017-01-26 07:40:22 -0500

    publication
    exemple 2.11

---
+++ Module02/EX16/IFT3100H17_DrawLine/src/application.h
@@ -0,0 +1,35 @@
+// IFT3100H17_DrawLine/application.h
+// Classe principale de l'application.
+
+#pragma once
+
+#include "ofMain.h"
+
+#include "renderer.h"
+
+class Application : public ofBaseApp
+{
+public:
+
+  Renderer * renderer;
+
+  bool isVerbose;
+
+  Application();
+
+  void setup();
+  void draw();
+
+  void mouseMoved(int x, int y);
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+
+  void keyReleased(int key);
+
+  void exit();
+
+  ~Application();
+};
