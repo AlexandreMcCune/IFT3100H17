b9e5841ae5c3c49ce6ae5e528d94f18f44780164
philvoyer <philvoyer@gmail.com>
2017-03-22 19:50:08 -0400

    publication
    exemple 6.1

---
+++ Module06/EX01/IFT3100H17_Variable.glsl
@@ -0,0 +1,21 @@
+// IFT3100H17_Variable.glsl
+// Exemples de déclaration de variables en GLSL.
+
+// exemple 1 : différentes configurations du niveau de précision des variables numériques entières
+precision lowp int;
+precision mediump int;
+precision highp int;
+
+// exemple 2 : différentes configurations du niveau de précision des variables numériques réelles
+precision lowp float;
+precision mediump float;
+precision highp float;
+
+// exemple 3 : déclaration d'un nombre scalaire avec une dimension (nombre entier)
+int i = 1;
+
+// exemple 4 : déclaration d'un nombre scalaire avec une dimension (nombre réel)
+float f = 1.0;
+
+// exemple 5 : déclaration d'une variable booléenne
+bool flag = true;
